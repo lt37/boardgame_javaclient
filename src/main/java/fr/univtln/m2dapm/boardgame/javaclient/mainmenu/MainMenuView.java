80d861c16a3ee8156c0ab44e1c9d94b14db9b206
Lois Tonnet <tonnetlois@gmail.com>
2018-01-20 14:57:52 +0100

    Added main menu view

---
+++ src/main/java/fr/univtln/m2dapm/boardgame/javaclient/mainmenu/MainMenuView.java
@@ -0,0 +1,62 @@
+package fr.univtln.m2dapm.boardgame.javaclient.mainmenu;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import static fr.univtln.m2dapm.boardgame.javaclient.ViewConstants.*;
+
+public class MainMenuView {
+
+    private Stage stage;
+    private Scene scene;
+    private VBox mainPane;
+
+    private Text gameTitle;
+    private Button createButton;
+    private Button joinButton;
+
+    private final String CREATE = "Créer une partie";
+    private final String JOIN = "Rejoindre une partie";
+    private final String SCENE_TITLE = "Menu Principal";
+
+
+    public MainMenuView(Stage stage) {
+        this.stage = stage;
+
+        initializeElements();
+        initializeMainPane();
+        initializeScene();
+        setListeners();
+
+        stage.show();
+    }
+
+
+    private void initializeElements() {
+        gameTitle = new Text(GAME_TITLE);
+        createButton = new Button(CREATE);
+        joinButton = new Button(JOIN);
+    }
+
+
+    private void initializeMainPane() {
+        mainPane = new VBox();
+        mainPane.setSpacing(SMALL_SPACING);
+        mainPane.setAlignment(Pos.CENTER);
+        mainPane.getChildren().addAll(gameTitle, createButton, joinButton);
+    }
+
+
+    private void initializeScene() {
+        scene = new Scene(mainPane, MAIN_MENU_WIDTH, MAIN_MENU_HEIGHT);
+        stage.setTitle(APP_TITLE + TITLE_SEPARATION + SCENE_TITLE);
+        stage.setScene(scene);
+    }
+
+
+    private void setListeners() {}
+}
